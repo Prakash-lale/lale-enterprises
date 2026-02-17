a950ba48af03574b728dd42dc94ac7b53598a080
Prakash-lale <prakashlaleabc@gmail.com>
2026-02-17 17:49:47 +0530

    fix: 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://Prakash-lale.github.io',
-  base: '/lale-enterprises/',
+  base: '/',
   integrations: [
     react(),
     sitemap(),
